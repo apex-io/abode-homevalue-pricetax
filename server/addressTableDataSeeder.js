66bba83f5753bcaf3aeab6eb42abbb27cfb27755
Phuc Tran <phuctranlv@gmail.com>
2020-03-13 15:56:59 -0700

    Refactor seeding function and seperate seeing from model

--- server/addressTableDataSeeder.js
+++ server/addressTableDataSeeder.js
@@ -1,35 +1,24 @@
 const faker = require('faker');
+const fs = require('fs');
 
-const addresses = {};
-
-addresses.address = [];
+const addresses = {
+  address: [],
+  on_market: [],
+  sqft: [],
+  bed: [],
+  bath: [],
+  addressesTableData: '',
+};
 
 for (let i = 0; i < 100; i += 1) {
   addresses.address.push(faker.fake('{{address.streetAddress}} {{address.streetName}}, {{address.city}}, {{address.state}}, {{address.zipCode}} '));
-}
-
-addresses.on_market = [];
-
-for (let i = 0; i < 100; i += 1) {
   addresses.on_market.push(faker.random.boolean());
-}
-
-addresses.sqft = [];
-
-for (let i = 0; i < 100; i += 1) {
   addresses.sqft.push(faker.random.number({ min: 200, max: 7000 }));
-}
-
-addresses.bed = [];
-
-for (let i = 0; i < 100; i += 1) {
   addresses.bed.push(faker.random.number({ min: 0, max: 10 }));
-}
-
-addresses.bath = [];
-
-for (let i = 0; i < 100; i += 1) {
   addresses.bath.push(faker.random.number({ min: 1, max: 10 }));
+  addresses.addressesTableData += `"\\N"\t${addresses.address[i]}\t${addresses.on_market[i]}\t${addresses.sqft[i]}\t${addresses.bed[i]}\t${addresses.bath[i]}\t\n`;
 }
+
+fs.writeFile('addressTableData.txt', addresses.addressesTableData, 'utf8');
 
 module.exports = { addresses };
