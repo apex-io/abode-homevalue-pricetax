a8a2fb219e737cffb4720d1f9c65011a2d82deaa
Phuc Tran <phuctranlv@gmail.com>
2020-03-15 14:36:02 -0700

    Complete environment set up for testing
    This commit complete the set up for enzyme testing environment using
    jest test runner.
    
    This testing environment also takes in consideration of webpack and babel
    usage in the project.
    
    ESLint is also set up for this project using airbnb style.

--- jest.config.js
+++ jest.config.js
@@ -1,48 +1,21 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which
-  // coverage information should be collected
   collectCoverageFrom: ['**/*.{js,jsx}'],
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // An array of file extensions your modules use
   moduleFileExtensions: ['js', 'json', 'jsx'],
-
-  // The paths to modules that run some code to configure or set up
-  // the testing environment before each test
   setupFiles: ['./enzyme.config.js'],
-
-  // The test environment that will be used for testing
-  testEnvironment: 'jsdom',
-
-
-  // An array of regexp pattern strings that are matched against all
-  // test paths, matched tests are skipped
+  setupFilesAfterEnv: ['jest-enzyme'],
+  testEnvironment: 'enzyme',
+  testEnvironmentOptions: {
+    enzymeAdapter: 'react16',
+  },
   testPathIgnorePatterns: ['\\\\node_modules\\\\', '\\\\coverage\\\\'],
-
-  // This option sets the URL for the jsdom environment. It is reflected
-  // in properties such as location.href
   testURL: 'http://localhost',
-
-  // transform: {
-  //   '^.+\\.js$': 'babel-jest',
-  //   '^.+\\.jsx$': 'babel-jest',
-  // },
-
-  // An array of regexp pattern strings that are matched against all
-  // source file paths, matched files will skip transformation
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.jsx$': 'babel-jest',
+  },
   transformIgnorePatterns: ['./node_modules/'],
-
-  // Indicates whether each individual test should be reported during the run
   verbose: false,
 };
