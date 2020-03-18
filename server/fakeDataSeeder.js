784d9a99c9961c9b118db84e0835890124be532d
Phuc Tran <phuctranlv@gmail.com>
2020-03-17 22:51:52 -0700

    Increase the number of fake data point
    Increase the number of houses in the database for more
    houses when building the home value estimate model
    
    Increase the number of values in the database per house
    so the graph of values can be smoother

--- server/fakeDataSeeder.js
+++ server/fakeDataSeeder.js
@@ -20,7 +20,10 @@ const valueHistory = {
   estimatedValueHistoryTableData: '',
 };
 
-for (let i = 0; i < 100; i += 1) {
+const numberOfHouses = 1000;
+const numberOfValuesPerHouse = 1000;
+
+for (let i = 0; i < numberOfHouses; i += 1) {
   addresses.address.push(faker.fake('{{address.streetAddress}} {{address.streetName}}, {{address.city}}, {{address.state}}, {{address.zipCode}} '));
   addresses.on_market.push(faker.random.boolean());
   addresses.sqft.push(faker.random.number({ min: 200, max: 7000 }));
@@ -34,10 +37,12 @@ for (let i = 0; i < 100; i += 1) {
   valueHistory.cityValue.push([]);
   if (i === 0) {
     valueHistory.centralValue.push(200000);
-  } else {
+  } else if (i % 10 === 0) {
     valueHistory.centralValue.push(valueHistory.centralValue[i - 1] * (102 / 100));
+  } else {
+    valueHistory.centralValue.push(valueHistory.centralValue[i - 1]);
   }
-  for (let j = 0; j < 100; j += 1) {
+  for (let j = 0; j < numberOfValuesPerHouse; j += 1) {
     const date = faker.date.between('1/1/2010', '1/1/2020');
     const convertedDate = `${(new Date(date)).getFullYear()}-${(new Date(date)).getMonth() + 1}-${(new Date(date)).getDate()}`;
     valueHistory.date[i].push(convertedDate);
@@ -46,25 +51,38 @@ for (let i = 0; i < 100; i += 1) {
         min: valueHistory.centralValue[i] * (98 / 100),
         max: valueHistory.centralValue[i] * (102 / 100),
       }));
-    } else {
+    } else if (j % 10 === 0) {
       valueHistory.homeValue[i].push(faker.random.number({
         min: valueHistory.homeValue[i][j - 1] * (98 / 100),
         max: valueHistory.homeValue[i][j - 1] * (102 / 100),
       }));
+      valueHistory.areaValue[i].push(faker.random.number({
+        min: valueHistory.homeValue[i][j] * (96 / 100),
+        max: valueHistory.homeValue[i][j] * (104 / 100),
+      }));
+      valueHistory.cityValue[i].push(faker.random.number({
+        min: valueHistory.homeValue[i][j] * (94 / 100),
+        max: valueHistory.homeValue[i][j] * (106 / 100),
+      }));
+    } else {
+      valueHistory.homeValue[i].push(faker.random.number({
+        min: valueHistory.homeValue[i][j - 1] * (99.98 / 100),
+        max: valueHistory.homeValue[i][j - 1] * (100.02 / 100),
+      }));
+      valueHistory.areaValue[i].push(faker.random.number({
+        min: valueHistory.homeValue[i][j] * (99.98 / 100),
+        max: valueHistory.homeValue[i][j] * (100.02 / 100),
+      }));
+      valueHistory.cityValue[i].push(faker.random.number({
+        min: valueHistory.homeValue[i][j] * (99.98 / 100),
+        max: valueHistory.homeValue[i][j] * (100.02 / 100),
+      }));
     }
-    valueHistory.areaValue[i].push(faker.random.number({
-      min: valueHistory.homeValue[i][j] * (96 / 100),
-      max: valueHistory.homeValue[i][j] * (104 / 100),
-    }));
-    valueHistory.cityValue[i].push(faker.random.number({
-      min: valueHistory.homeValue[i][j] * (94 / 100),
-      max: valueHistory.homeValue[i][j] * (106 / 100),
-    }));
   }
 }
-for (let i = 0; i < 100; i += 1) {
+for (let i = 0; i < numberOfHouses; i += 1) {
   valueHistory.date[i].sort(-1);
-  for (let j = 0; j < 100; j += 1) {
+  for (let j = 0; j < numberOfValuesPerHouse; j += 1) {
     valueHistory.estimatedValueHistoryTableData += `"\\N"\t${valueHistory.address[i]}\t${valueHistory.date[i][j]}\t${valueHistory.homeValue[j][i]}\t${valueHistory.areaValue[j][i]}\t${valueHistory.cityValue[j][i]}\t\n`;
   }
 }
