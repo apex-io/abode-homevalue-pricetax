4e8f6c0e9cc1a0c92f6919e6937d8bd3f0725927
Phuc Tran <phuctranlv@gmail.com>
2020-03-20 21:04:04 -0700

    Assign className to elements

--- client/components/ComparableHomeModelEntry.jsx
+++ client/components/ComparableHomeModelEntry.jsx
@@ -1,10 +1,11 @@
 const React = require('react');
+import styles from '../style/ComparableHomeModelEntry.css';
 
 class ComparableHomeModelEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: true,
+      show: true
     };
   }
 
@@ -12,13 +13,27 @@ class ComparableHomeModelEntry extends React.Component {
     return (
       // test if the below render
       // <div>{JSON.stringify(this.props.addressSummary)}</div>
-      <div>
-        <div>Address: {this.props.addressSummary.address}</div>
-        <div>For sale: {this.props.addressSummary.on_market}</div>
-        <div>square foot: {this.props.addressSummary.sqft}</div>
-        <div>bed: {this.props.addressSummary.bed}</div>
-        <div>bath: {this.props.addressSummary.bath}</div>
-        <div>Estimate value: ${this.props.addressSummary.currentestimatedvalue}</div>
+      <div className={styles.ComparableHomeModelEntry}>
+        {/* <div>Address: {this.props.addressSummary.address}</div> */}
+        <div>
+          <img className={styles.image} src="https://photos.zillowstatic.com/p_h/ISbd8ki9klk0ki1000000000.jpg"></img>
+        </div>
+        <div className={styles.homeSummary}>
+          <div className={styles.homeValue}>
+            <div>${new Intl.NumberFormat().format(this.props.addressSummary.currentestimatedvalue)}</div>
+            <div className={styles.forSale}>sale: {this.props.addressSummary.on_market}</div>
+          </div>
+          <div className={styles.homeValuePerSqft}>
+            ${new Intl.NumberFormat().format((this.props.addressSummary.currentestimatedvalue / this.props.addressSummary.sqft).toFixed(2))}/sqft
+            </div>
+          <div className={styles.homeDetails}>
+            <span className={styles.detailNumber}>{this.props.addressSummary.bed}</span> <span> bd</span>
+            <div className={styles.detailSpacing}></div>
+            <span className={styles.detailNumber}>{this.props.addressSummary.bath}</span><span> ba</span>
+            <div className={styles.detailSpacing}></div>
+            <span className={styles.detailNumber}>{this.props.addressSummary.sqft}</span> <span> sqft</span>
+          </div>
+        </div>
       </div>
     );
   }
