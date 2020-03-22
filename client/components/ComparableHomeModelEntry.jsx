d571a8e7e73526c6d46f11aaf0fafa56757b7206
Phuc Tran <phuctranlv@gmail.com>
2020-03-22 00:22:24 -0700

    Add onClickHandler to request data for clicked on address

--- client/components/ComparableHomeModelEntry.jsx
+++ client/components/ComparableHomeModelEntry.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 import styles from '../style/ComparableHomeModelEntry.css';
+const $ = require('jquery');
 
 class ComparableHomeModelEntry extends React.Component {
   constructor(props) {
@@ -13,13 +14,32 @@ class ComparableHomeModelEntry extends React.Component {
         saleIcon: "Gray_Light_Icon.svg"
       };
     }
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  onClickHandler(event) {
+    // test if the correct data is being sent
+    $.ajax({
+      type: 'get',
+      url: '/exampleHomeSummary/',
+      data: {
+        address: this.props.addressSummary.address,
+        zipCode: this.props.addressSummary.zipcode,
+      },
+      success: (result) => {
+        this.props.onClickNewAddressShowMoreHandler();
+        this.props.onClickNewAddressHandler(result);
+        window.scrollTo(0, 0);
+      },
+    });
+    event.preventDefault();
   }
 
   render() {
     return (
       // test if the below render
       // <div>{JSON.stringify(this.props.addressSummary)}</div>
-      <div className={styles.ComparableHomeModelEntry}>
+      <div className={styles.ComparableHomeModelEntry} onClick={this.onClickHandler}>
         {/* <div>Address: {this.props.addressSummary.address}</div> */}
         <div className={styles.upperSide}>
           <img className={styles.image} src="https://photos.zillowstatic.com/p_h/ISbd8ki9klk0ki1000000000.jpg"></img>
