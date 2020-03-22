b464d1b829f7ff9fbcab551e336c22a7d4fc9ac9
Phuc Tran <phuctranlv@gmail.com>
2020-03-21 22:55:24 -0700

    Add icons for estimate models

--- client/components/HomeValueApp.jsx
+++ client/components/HomeValueApp.jsx
@@ -72,14 +72,21 @@ class HomeValueApp extends React.Component {
               <div className={styles.bestimateRange}>Bestimate sale range: ${new Intl.NumberFormat().format(bestimateRangeLow)} - ${new Intl.NumberFormat().format(bestimateRangeHigh)}</div>
             </div>
           </div>
-          <button onClick={this.onClickHandler} className={styles.showEstimateModelsButton}>See more estimated models</button>
+          <button onClick={this.onClickHandler} className={styles.showEstimateModelsButton}><img className={styles.icon} src="down-arrow.svg"></img>See more estimated models</button>
           
           <div style={this.state.showEstimateModels} className={styles.bestimateModelsSection}>
             <div>
               <div className={styles.bestimateModelTitle}>Bestimate models</div>
               <div className={styles.bestimateModelExplaination}>The Bestimate uses a set of data models to estimate this home's value.</div>
               <div className={styles.differentModels}>
-                <span className={styles.modelNames}>Comparable homes</span><span className={styles.modelNames}>Off-market model</span>
+                <span className={styles.modelNames}>
+                  <img className={styles.iconWithSpace} src="iconfinder_House_4265801.svg"></img>
+                  <span> Comparable homes</span>
+                </span>
+                <span className={styles.modelNames}>
+                  <img className={styles.iconWithSpace} src="iconfinder_m-21_4230540.svg"></img>
+                  <span> Off-market model</span>
+                </span>
               </div>
             </div>
             {/* for test: show the component below rendered and if the error is at this level or at the child level */}
